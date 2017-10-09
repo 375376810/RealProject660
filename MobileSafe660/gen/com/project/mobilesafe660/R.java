ac5020198c2d9e8207f4d41f7acbfd4b65ea3b0b
袁星明 <375376810@qq.com>
2017-10-09 19:57:32 +0800

    界面已展示

--- MobileSafe660/gen/com/project/mobilesafe660/R.java
+++ MobileSafe660/gen/com/project/mobilesafe660/R.java
@@ -23,12 +23,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int launcher_bg=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int pb_loading=0x7f080001;
+        public static final int tv_name=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_splash=0x7f030000;
     }
     public static final class menu {
         public static final int main=0x7f070000;
---
+++ MobileSafe660/src/com/project/mobilesafe660/activity/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.project.mobilesafe660.activity;
+
+import com.project.mobilesafe660.R;
+import com.project.mobilesafe660.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
