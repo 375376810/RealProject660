f6d853c423541e974e4a0104ee5c304194dc9729
袁星明 <375376810@qq.com>
2017-10-10 17:31:21 +0800

    2.0版本,增加了闪屏页面和主页面标题栏

---
+++ MobileSafe660/src/com/project/mobilesafe660/activity/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.project.mobilesafe660.activity;
+
+import com.project.mobilesafe660.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/***
+ * @author Ô¬ÐÇÃ÷
+ * Ö÷Ò³Ãæ
+ */
+public class HomeActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+}
---
+++ MobileSafe660/src/com/project/mobilesafe660/utils/StreamUtils.java
@@ -0,0 +1,21 @@
+package com.project.mobilesafe660.utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class StreamUtils {
+	
+	public static String streamToString(InputStream in) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		int len = 0;
+		while ((len = in.read(buffer))!=-1) {
+			out.write(buffer,0,len);
+		}
+		in.close();
+		out.close();
+		return out.toString();
+	}
+
+}
